bc67d64b6abe7f1a1354377ccbd7e4395e484c66
Kenneth McEastland <kennethmceastland@Kenneths-MacBook-Air.local>
2020-05-19 18:19:31 -0500

    added auth & Data Base

--- src/components/Auth/Auth.js
+++ src/components/Auth/Auth.js
@@ -1,16 +1,23 @@
 import React from 'react';
+import firebase from 'firebase/app';
+import 'firebase/auth';
 
 import './Auth.scss';
 
-class Auth extends React.component {
+class Auth extends React.Component {
+  loginClickEvent = (e) => {
+    e.preventDefault();
+    const provider = new firebase.auth.GoogleAuthProvider();
+    firebase.auth().signInWithPopup(provider);
+  }
+
   render() {
     return (
       <div className="Auth">
-        <h1>Inside Auth Component</h1>
+        <button className="btn btn-danger" onClick={this.loginClickEvent}>Google Auth</button>
       </div>
     );
   }
 }
-
 
 export default Auth;
