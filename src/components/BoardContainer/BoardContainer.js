3d0854091db23152e01349f7283b05a44f0e226f
Kenneth McEastland <kennethmceastland@Kenneths-MacBook-Air.local>
2020-05-20 19:17:38 -0500

    got cards to print

--- src/components/BoardContainer/BoardContainer.js
+++ src/components/BoardContainer/BoardContainer.js
@@ -1,12 +1,40 @@
-
 import React from 'react';
+import PropTypes from 'prop-types';
+
 import './BoardContainer.scss';
 
+import boardsData from '../../helpers/data/boardsData';
+import authData from '../../helpers/data/authData';
+
+import Board from '../Board/Board';
+
 class BoardContainer extends React.Component {
+  static propTypes = {
+    setSingleBoard: PropTypes.func.isRequired,
+  }
+
+  state = {
+    boards: [],
+  }
+
+  componentDidMount() {
+    boardsData.getBoardsByUid(authData.getUid())
+      .then((boards) => this.setState({ boards }))
+      .catch((err) => console.error('unable to get all boards: ', err));
+  }
+
   render() {
+    const { boards } = this.state;
+    const { setSingleBoard } = this.props;
+
+    const makeBoards = boards.map((board) => <Board key={board.id} board={board} setSingleBoard={setSingleBoard}/>);
+
     return (
       <div className="BoardContainer">
-        <h1>INSIDE BOARD CONTAINER COMPONENT</h1>
+        <h2>Boards</h2>
+        <div className="d-flex flex-wrap">
+          {makeBoards}
+        </div>
       </div>
     );
   }
